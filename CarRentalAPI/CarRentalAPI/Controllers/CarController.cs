facb3e669ac634f2bf96d6325df4b067bf17ab86
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-05-25 18:00:52 +0200

    [Add] controllers
    CarController, ReservationController, ClientController

---
+++ CarRentalAPI/CarRentalAPI/Controllers/CarController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRentalAPI.Data.Model;
+using CarRentalAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private CarService _carService;
+
+        public CarController(CarService carService)
+        {
+            _carService = carService;
+        }
+        
+        // GET: api/Car
+        [HttpGet]
+        public IEnumerable<Car> Get()
+        {
+            return _carService.ReadAll();
+        }
+
+        // GET: api/Car/5
+        [HttpGet("{id}")]
+        public ActionResult<Car> Get(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            Car car = _carService.Read(id);
+            if (car == null)
+                return BadRequest("There's no car with such an id");
+            return Ok(car);
+        }
+
+        // POST: api/Car
+        [HttpPost]
+        public IActionResult Post([FromBody] Car car)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            int id = _carService.Create(car);
+            return Created("api/Car", id);
+        }
+
+        // PUT: api/Car/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            try
+            {
+                _carService.Delete(id);
+            }
+            catch(ArgumentNullException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+
+            return Ok("Successfully deleted");
+        }
+    }
+}
---
+++ CarRentalAPI/CarRentalAPI/Controllers/ReservationController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRentalAPI.Data.Model;
+using CarRentalAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private ReservationService _reservationService;
+
+        public ReservationController(ReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+        
+        // GET: api/Reservation
+        [HttpGet]
+        public IEnumerable<Reservation> Get()
+        {
+            return _reservationService.ReadAll();
+        }
+
+        // GET: api/Reservation/5
+        [HttpGet("{id}")]
+        public ActionResult<Reservation> Get(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            Reservation reservation = _reservationService.Read(id);
+            if (reservation == null)
+                return BadRequest("There's no reservation with such an id");
+            return Ok(reservation);
+        }
+
+        // POST: api/Reservation
+        [HttpPost]
+        public IActionResult Post([FromBody] Reservation reservation)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            int id = _reservationService.Create(reservation);
+            return Created("api/Reservation", id);
+        }
+
+        // PUT: api/Reservation/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            try
+            {
+                _reservationService.Delete(id);
+            }
+            catch (ArgumentNullException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+
+            return Ok("Successfully deleted");
+        }
+    }
+}
