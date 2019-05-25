70907a30b4c2fa689327c688f9be2b899e99a223
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-05-25 16:38:09 +0200

    [Add] services
    Simple CREATE, READ, DELETE for Cars, Reservations, Clients

---
+++ CarRentalAPI/CarRentalAPI/Services/CarService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CarRentalAPI.Data;
+using CarRentalAPI.Data.Model;
+
+namespace CarRentalAPI.Services
+{
+    public class CarService
+    {
+        private CarDbContext _carDbContext;
+
+        public CarService(CarDbContext carDbContext)
+        {
+            _carDbContext = carDbContext;
+        }
+
+        public int Create(Car car)
+        {
+            _carDbContext.Cars.Add(car);
+            _carDbContext.SaveChanges();
+            return car.Id;
+        }
+
+        public List<Car> ReadAll()
+        {
+            return _carDbContext.Cars.ToList();
+        }
+
+        public Car Read(int id)
+        {
+            return _carDbContext.Cars.SingleOrDefault(car=>car.Id==id);
+        }
+
+        public void Delete(int id)
+        {
+            Car car = Read(id);
+            if (car == null)
+                throw new ArgumentNullException("There's no car with such an id");
+            _carDbContext.Cars.Remove(car);
+            _carDbContext.SaveChanges();
+        }
+    }
+}
---
+++ CarRentalAPI/CarRentalAPI/Services/ReservationService.cs
@@ -0,0 +1,45 @@
+﻿using CarRentalAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRentalAPI.Data.Model;
+
+namespace CarRentalAPI.Services
+{
+    public class ReservationService
+    {
+        private CarDbContext _carDbContext;
+
+        public ReservationService(CarDbContext carDbContext)
+        {
+            _carDbContext = carDbContext;
+        }
+
+        public int Create(Reservation reservation)
+        {
+            _carDbContext.Reservations.Add(reservation);
+            _carDbContext.SaveChanges();
+            return reservation.Id;
+        }
+
+        public List<Reservation> ReadAll()
+        {
+            return _carDbContext.Reservations.ToList();
+        }
+
+        public Reservation Read(int id)
+        {
+            return _carDbContext.Reservations.SingleOrDefault(reservation => reservation.Id == id);
+        }
+
+        public void Delete(int id)
+        {
+            Reservation reservation = Read(id);
+            if (reservation == null)
+                throw new ArgumentNullException("There's no reservation with such an id");
+            _carDbContext.Reservations.Remove(reservation);
+            _carDbContext.SaveChanges();
+        }
+    }
+}
