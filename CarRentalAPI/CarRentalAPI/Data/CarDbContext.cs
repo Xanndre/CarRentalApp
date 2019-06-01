359a81f930d7d0b76c260a54be83aca67809b14e
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-06-01 15:40:38 +0200

    [Change] car images' URLs

--- CarRentalAPI/CarRentalAPI/Data/CarDbContext.cs
+++ CarRentalAPI/CarRentalAPI/Data/CarDbContext.cs
@@ -20,9 +20,9 @@ namespace CarRentalAPI.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Car>().HasData(new[]{
-                    new Car{ Id = 1, Brand = "test1", Cost = 31, Description = "Simple description1", ImageUrl = "Url1" },
-                    new Car{ Id = 2, Brand = "test2", Cost = 32, Description = "Simple description2", ImageUrl = "Url2" },
-                    new Car{ Id = 3, Brand = "test3", Cost = 33, Description = "Simple description3", ImageUrl = "Url3" }
+                    new Car{ Id = 1, Brand = "test1", Cost = 31, Description = "Simple description1", ImageUrl = "https://hdqwalls.com/download/audi-r8-1366x768.jpg" },
+                    new Car{ Id = 2, Brand = "test2", Cost = 32, Description = "Simple description2", ImageUrl = "http://s1.1zoom.me/b5050/715/BMW_M5_Blue_Sedan_549904_1366x768.jpg" },
+                    new Car{ Id = 3, Brand = "test3", Cost = 33, Description = "Simple description3", ImageUrl = "http://s1.1zoom.me/b5364/28/Renault_Trophy_2015_Renaultsport_Clio_White_525674_1366x768.jpg" }
                 }
             );
         }
---
+++ CarRentalAPI/CarRentalAPI/Migrations/20190601130406_CarDataUpdate.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRentalAPI.Migrations
+{
+    public partial class CarDataUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://hdqwalls.com/download/audi-r8-1366x768.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "http://s1.1zoom.me/b5050/715/BMW_M5_Blue_Sedan_549904_1366x768.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "http://s1.1zoom.me/b5364/28/Renault_Trophy_2015_Renaultsport_Clio_White_525674_1366x768.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "Url1");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "Url2");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "Url3");
+        }
+    }
+}
